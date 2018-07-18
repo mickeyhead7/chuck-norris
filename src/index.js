41f3d509dac55978952452b32ed6c6feb6065900
Mark Head <mickeyhead7@gmail.com>
2018-07-18 01:16:42 +0100

    Moved store creation

--- src/index.js
+++ src/index.js
@@ -1,20 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { applyMiddleware, createStore } from 'redux';
-import createSagaMiddleware from 'redux-saga';
-import reducers from './store/reducers';
-import sagas from './store/sagas'
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-
-const sagaMiddleware = createSagaMiddleware();
-const store = createStore(
-  reducers,
-  applyMiddleware(sagaMiddleware)
-);
-
-sagaMiddleware.run(sagas);
+import store from './store';
 
 ReactDOM.render(
   <Provider store={store}>
